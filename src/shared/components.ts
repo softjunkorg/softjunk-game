96da1307c0a1562beed0332a85dd3cd0f8ff5e1a
felpsdev <felaxx.developer@gmail.com>
2022-09-01 22:40:32 -0300

    fix component minimap

--- src/shared/components.ts
+++ src/shared/components.ts
@@ -1,38 +1,25 @@
 import "@citizenfx/client";
 
 export function GetMinimapPosition() {
-    const [resX, resY] = GetActiveScreenResolution();
-    const aspectRatio = GetAspectRatio(true);
-    const scaleX = 1 / resX;
-    const scaleY = 1 / resY;
-    const minimap = {
-        width: 0,
-        height: 0,
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-        X: 0,
-        Y: 0,
+    const [x, y] = GetActiveScreenResolution();
+    const aspect_ratio = GetAspectRatio(false);
+    const hidden = IsRadarHidden();
+    const safezone = GetSafeZoneSize();
+
+    const safezone_x = 1.0 / 20.0;
+    const safezone_y = 1.0 / 20.0;
+    const xscale = 1.0 / x;
+    const yscale = 1.0 / y;
+
+    const width = xscale * (x / (4 * aspect_ratio)) * x;
+    const height = !hidden ? yscale * (y / 5.674) * y : 0;
+
+    return {
+        width,
+        height,
+        X: xscale * (x * (safezone_x * (Math.abs(safezone - 1.0) * 10))) * x,
+        Y:
+            !hidden &&
+            yscale * (y * (safezone_y * (Math.abs(safezone - 1.0) * 10))) * y,
     };
-
-    /* GFX */
-    SetScriptGfxAlign(76, 66);
-    const [minimapRawX, minimapRawY] = GetScriptGfxPosition(
-        -0.005,
-        0.004 + -0.188888,
-    );
-    minimap.width = scaleX * (resX / (4 * aspectRatio)) * resX;
-    minimap.height = scaleY * (resY / 5.674) * resY;
-    ResetScriptGfxAlign();
-
-    /* Positions */
-    minimap.left = minimapRawX * resX + 10;
-    minimap.right = minimapRawX + minimap.width;
-    minimap.top = minimapRawY * resY + 10;
-    minimap.bottom = minimapRawY + minimap.height;
-    minimap.X = minimapRawX + minimap.width / 2;
-    minimap.Y = minimapRawY + minimap.height / 2;
-
-    return minimap;
 }
